d446ff9b191b4f8eaec7b13bff0f0f35c04a82f5
matiasberrios <berriosmatias@gmail.com>
2023-11-16 00:14:30 -0300

    massive progress

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ChatService } from '../chat.service';
 import { firstValueFrom } from 'rxjs';
-
+import { ViewChild } from '@angular/core';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
@@ -9,28 +9,76 @@ import { firstValueFrom } from 'rxjs';
 })
 export class ChatComponent {
   constructor(private chatService: ChatService) {}
-  messages: Message[] = [];
+  messages: Message[] = [
+    {
+      content: 'Hello, I am a chatbot. How can I help you?',
+      sender: 'yours',
+    },
+    {
+      content: 'Hello, I am a chatbot. How can I help you?',
+      sender: 'yours',
+    },
+    {
+      content: 'Hello, I am a chatbot. How can I help you?',
+      sender: 'yours',
+    },
+    {
+      content: 'Hello, I am a chatbot. How can I help you?',
+      sender: 'yours',
+    },
+    {
+      content: 'Hello, I am a chatbot. How can I help you?',
+      sender: 'yours',
+    },
+    {
+      content: 'Hello, I am a chatbot. How can I help you?',
+      sender: 'yours',
+    },
+    {
+      content: 'Hello, I am a chatbot. How can I help you?',
+      sender: 'mine',
+    },
+    {
+      content: 'Hello, I am a chatbot. How can I help you?',
+      sender: 'yours',
+    },
+  ];
   newMessageContent: string = '';
   loading: boolean = false;
+  muted: boolean = false;
+
+  @ViewChild('chatContainer') chatContainer: any;
 
   async sendMessage() {
-    if (this.newMessageContent.trim()) {
+    if (this.newMessageContent.trim() && !this.loading) {
       this.messages.push({
         content: this.newMessageContent,
         sender: 'mine',
       });
       this.loading = true;
-      this.messages.push({
+      this.playSound('sent');
+
+      const placeholderMessage: Message = {
         content: '...',
         sender: 'yours',
-      });
+      };
+      this.messages.push(placeholderMessage);
+
       firstValueFrom(this.chatService.sendMessage(this.newMessageContent)).then(
-        (response) => {
+        (response: any) => {
           this.loading = false;
-          console.log(response);
+          const messageContent = response.messages[0].content[0].text.value;
+          placeholderMessage.content = messageContent;
+          setTimeout(() => {
+            this.scrollToBottom();
+          }, 100);
+          this.playSound('recieved');
         }
       );
       this.newMessageContent = '';
+      setTimeout(() => {
+        this.scrollToBottom();
+      }, 100);
     }
   }
 
@@ -46,6 +94,24 @@ export class ChatComponent {
       }
       return acc;
     }, [] as Message[][]);
+  }
+
+  private scrollToBottom(): void {
+    try {
+      this.chatContainer.nativeElement.scrollTop =
+        this.chatContainer.nativeElement.scrollHeight * 100;
+    } catch (err) {
+      console.error('Error while scrolling:', err);
+    }
+  }
+
+  playSound(file: 'sent' | 'recieved') {
+    if (!this.muted) {
+      const audio = new Audio();
+      audio.src = `../../assets/${file}.mp3`;
+      audio.load();
+      audio.play();
+    }
   }
 }
 
